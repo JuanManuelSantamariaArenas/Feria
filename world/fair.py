e7146c7edc23cd605ebb4fa611e99d6019822246
Juan Manuel Santamaria Arenas <juanmanuelsantamariaarenas01@gmail.com>
2023-11-27 22:04:58 -0500

    Class correction

--- world/fair.py
+++ world/fair.py
@@ -1,20 +1,21 @@
 class Person:
-    def __init__(self, name: str, type_person: str, type_document: str, 
-                phone: str, mail: str, address: str) -> None:
+    def __init__(self) -> None:
         print("Person created...")
-        self.name = None
-        self.type_person = None
-        self.type_document = None
-        self.phone = None
-        self.mail = None
-        self.address = None
+        self.name: str = None
+        self.type_person: str = None
+        self.type_document: str = None
+        self.phone: str = None
+        self.mail: str = None
+        self.address: str = None
 
 
 class Entrepreneur(Person):
     def __init__(self) -> None:
         super().__init__()
         print("Entrepreneur created...")
-
+    
+    def __str__(self) -> str:
+        return f"{self.name}, {self.type_person}, {self.type_document}, {self.phone}, {self.mail}, {self.address}"
 
 
 class Organizer(Person):
@@ -23,5 +24,14 @@ class Organizer(Person):
         print("Organizer created...")
         self.business_name = business_name
 
-entrepreneur_one = Entrepreneur()
-print(entrepreneur_one.name)
+def test(nombre, pito, tipo, celu, mail, dir):
+    entrepreneur_one = Entrepreneur()
+    entrepreneur_one.name = nombre
+    entrepreneur_one.type_person = pito
+    entrepreneur_one.type_document = tipo
+    entrepreneur_one.phone = celu
+    entrepreneur_one.mail = mail
+    entrepreneur_one.address = dir
+    print(entrepreneur_one)
+
+test("Luis", "Pi","CE", "ra", "pic", "lol")
